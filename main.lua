de12f5b38c74562d9043aa420af4b1fbce95a306
shezan78 <97002070+shezan78@users.noreply.github.com>
2022-12-15 18:05:47 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ if (not game:IsLoaded()) then
     game.Loaded:Wait();
 end
 
-local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-esp/main/ui.lua"))();
+local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/shezanssp/main/main.lua"))();
 
 local PlaceId = game.PlaceId
 
